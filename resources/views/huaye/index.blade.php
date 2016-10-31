1da8770545f1b60c8ef2ed605aa63eda4fa0855f
hjl <89757630@qq.com>
2016-10-31 14:00:47 +0800

    update

--- resources/views/huaye/index.blade.php
+++ resources/views/huaye/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh-CN">
 <head>
     <meta charset="UTF-8">
-    <title>个人简历</title>
+    <title>CHAUWA CHAN - DESIGN</title>
     <!-- <meta name="viewport" content="initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no"> -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
